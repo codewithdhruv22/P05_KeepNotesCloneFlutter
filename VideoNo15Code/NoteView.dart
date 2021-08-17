c1c8d0d3c170f0bb134f0f440c1fca4ba212f811
Code With Dhruv <75844991+codewithdhruv22@users.noreply.github.com>
2021-08-17 18:59:19 +0530

    Code of video no 15

---
+++ VideoNo15Code/NoteView.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:keep_notes_clone_yt/ArchiveView.dart';
+import 'package:keep_notes_clone_yt/colors.dart';
+import 'package:keep_notes_clone_yt/home.dart';
+import 'package:keep_notes_clone_yt/services/db.dart';
+import 'EditNoteView.dart';
+import 'package:keep_notes_clone_yt/model/MyNoteModel.dart';
+class NoteView extends StatefulWidget {
+Note? note;
+NoteView({required this.note});
+
+  @override
+  _NoteViewState createState() => _NoteViewState();
+}
+
+class _NoteViewState extends State<NoteView> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        backgroundColor: bgColor,
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            splashRadius: 17,
+            onPressed: () async{
+await NotesDatabse.instance.pinNote(widget.note);
+Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Home()));
+            }, 
+            icon: Icon( widget.note!.pin ? Icons.push_pin :Icons.push_pin_outlined)),
+             IconButton(
+            splashRadius: 17,
+         onPressed: () async{
+await NotesDatabse.instance.archNote(widget.note);
+Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Home()));
+            } , 
+            icon: Icon(widget.note!.isArchieve ?Icons.archive :Icons.archive_outlined)),
+
+
+
+            IconButton(
+            splashRadius: 17,
+            onPressed: () async{
+              await NotesDatabse.instance.delteNote(widget.note);
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home()));
+            }, 
+            icon: Icon(Icons.delete_forever_outlined)),
+
+
+
+
+
+
+
+                 IconButton(
+            splashRadius: 17,
+            onPressed: () {
+
+              Navigator.push(context, MaterialPageRoute(builder: (context) => EditNoteView(note : widget.note)));
+            }, 
+            icon: Icon(Icons.edit_outlined))
+        ],
+      ),
+
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children : [
+            Text(widget.note!.title , style: TextStyle(color: Colors.white , fontSize: 23 , fontWeight: FontWeight.bold),),
+            SizedBox(height: 10,),
+            Text(widget.note!.content,style: TextStyle(color: Colors.white),)
+          ]
+          
+        ),
+      ),
+    );
+  }
+}
